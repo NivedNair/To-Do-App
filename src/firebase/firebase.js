443a5bfba326d8d4b9e9c08b85adae4d78e67990
NivedNair <nivedps1999@gmail.com>
2022-11-18 18:51:11 +0530

    Updated dependencies

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,15 +8,16 @@ import { getDatabase } from 'firebase/database'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB6xDXH2LBjaX1_e2VoZdmVBGZBiudAs-U",
-  authDomain: "todo-f850d.firebaseapp.com",
-  databaseURL: "https://todo-f850d-default-rtdb.firebaseio.com",
-  projectId: "todo-f850d",
-  storageBucket: "todo-f850d.appspot.com",
-  messagingSenderId: "972955069596",
-  appId: "1:972955069596:web:aa7b6dbebe48c4699dd159",
-  measurementId: "G-QX8P9PZX44"
+  apiKey: "AIzaSyAlFyRTOvKEdGJbPyqioKtK4BGxCDlE2MY",
+  authDomain: "assignment-368713.firebaseapp.com",
+  databaseURL: "https://assignment-368713-default-rtdb.firebaseio.com",
+  projectId: "assignment-368713",
+  storageBucket: "assignment-368713.appspot.com",
+  messagingSenderId: "657352866771",
+  appId: "1:657352866771:web:0648911dd967122ba9cd26",
+  measurementId: "G-L6SWJPYDKM"
 };
 
 // Initialize Firebase
